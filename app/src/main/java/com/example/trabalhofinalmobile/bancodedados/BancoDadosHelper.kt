1702073d9e32a325e991a51c373d2ce6b994bce2
CrisEngComp <cristinabernardessud78@gmail.com>
2025-07-13 18:55:04 -0300

    LivroDAO

--- app/src/main/java/com/example/trabalhofinalmobile/bancodedados/BancoDadosHelper.kt
+++ app/src/main/java/com/example/trabalhofinalmobile/bancodedados/BancoDadosHelper.kt
@@ -8,20 +8,25 @@ import java.sql.SQLException
 
 class BancoDadosHelper (context:Context):SQLiteOpenHelper (context, "EstanteInteligente", null, 1) {
 
+    //habilitação para o uso de foreign key
     override fun onConfigure(db: SQLiteDatabase?) {
         super.onConfigure(db)
         db?.setForeignKeyConstraintsEnabled(true)
     }
 
+    //criação do banco de dados
     override fun onCreate(db: SQLiteDatabase?) {
 
+        //criação da tabela usuário
         val sqlUsuario = "create table usuario (idUsuario integer not null primary key autoincrement, usuario varchar(50) not null, " +
                 "senha varchar(50) not null);"
 
+        //criação da tabela gênero
         val sqlGenero = "create table genero (idGenero integer not null primary key autoincrement, nomeGenero varchar(50) not null); "
 
+        //criação da tabela livro com relacionamento da tabela genero
         val sqlLivro = "create table livro (idLivro integer not null primary key autoincrement, titulo varchar(50), autor varchar(50)," +
-                "avaliacao interg not null, generoId integer not null, foreign key (generoId) references genero(idGenero));"
+                "avaliacao integer not null, idGenero integer not null, foreign key (idGenero) references genero(idGenero));"
 
         try {
             db?.execSQL(sqlUsuario)
@@ -35,12 +40,10 @@ class BancoDadosHelper (context:Context):SQLiteOpenHelper (context, "EstanteInte
         // Inserir gêneros iniciais
         val generosIniciais = listOf("Romance", "Fantasia", "Ficção Científica", "Suspense", "Aventura")
         generosIniciais.forEach { genero ->
-            db?.execSQL("insert into genero (nome) values ('$genero')")
+            db?.execSQL("insert into genero (nomeGenero) values ('$genero')")
         }
 
     }
-
-
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
---
+++ app/src/main/java/com/example/trabalhofinalmobile/bancodedados/LivroDAO.kt
@@ -0,0 +1,77 @@
+package com.example.trabalhofinalmobile.bancodedados
+
+import android.content.Context
+import android.util.Log
+import com.example.trabalhofinalmobile.classes.Livro
+import java.sql.SQLException
+
+class LivroDAO (context: Context) {
+
+    val escrita = BancoDadosHelper(context).writableDatabase
+    val leitura = BancoDadosHelper(context).readableDatabase
+
+    fun salvar(livro: Livro):Boolean{
+
+        var retorno = false
+        val sql = "insert into livro values (null, '${livro.titulo}, '${livro.autor}, ${livro.avaliacao}, ${livro.idGenero});"
+
+        try {
+            escrita.execSQL(sql)
+            retorno = true
+        }catch (e: SQLException){
+            Log.i("info_bd", "Erro ao salvar!")
+        }
+        return retorno
+    }
+
+    fun editar(livro: Livro):Boolean{
+
+        var retorno = false
+        val sql = "update livro set titulo = '${livro.titulo}', autor = '${livro.autor}', avaliacao = ${livro.avaliacao}, " +
+                "idGenero = ${livro.idGenero} where idLivro = ${livro.idLivro};"
+
+        try {
+            escrita.execSQL(sql)
+            retorno = true
+        }catch (e: SQLException){
+            Log.i("info_bd", "Erro ao editar!")
+        }
+        return retorno
+    }
+
+    fun excluir(livro: Livro): Boolean{
+
+        val sql = "delete from livro where idLivro = ${livro.idLivro};"
+
+        try {
+            escrita.execSQL(sql)
+        }catch (e:Exception){
+            Log.i("info_bd", "Erro ao excluir!")
+            return false
+        }
+        return true
+    }
+
+    fun listar():List<Livro>{
+
+        val listaLivro = mutableListOf<Livro>()
+        val sql = "select * from livro;"
+
+        try{
+            val cursor = leitura.rawQuery(sql, null)
+
+            while(cursor.moveToNext()){
+                val idLivro = cursor.getInt(0)
+                val titulo = cursor.getString(1)
+                val autor = cursor.getString(2)
+                val avaliacao = cursor.getInt(3)
+                val genero = cursor.getInt(4)
+
+                listaLivro.add(Livro(idLivro, titulo, autor, avaliacao, genero))
+            }
+        }catch (e:SQLException){
+            Log.i("info_bd", "Erro ao selecionar")
+        }
+        return listaLivro
+    }
+}
